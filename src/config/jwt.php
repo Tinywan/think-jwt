67c8f4fc613b1be25e54ac0d01213e20a8d82b08
Tinywan <756684177@qq.com>
2022-09-18 00:06:18 +0800

    Config File

--- src/config/jwt.php
+++ src/config/jwt.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @desc 异常配置文件
+ * @desc 配置文件
  * @author Tinywan(ShaoBo Wan)
  * @email 756684177@qq.com
  * @date 2022/9/11 15:56
@@ -8,68 +8,78 @@
 
 return [
     'enable' => true,
-    'jwt' => [
-        // 算法类型 HS256、HS384、HS512、RS256、RS384、RS512、ES256、ES384、Ed25519
-        'algorithms' => 'HS256',
-        // access令牌秘钥
-        'access_secret_key' => '2022d3d3LmJq',
-        // access令牌过期时间，单位：秒。默认 2 小时
-        'access_exp' => 7200,
-        // refresh令牌秘钥
-        'refresh_secret_key' => '2022KTxigxc9o50c',
-        // refresh令牌过期时间，单位：秒。默认 7 天
-        'refresh_exp' => 604800,
-        // refresh 令牌是否禁用，默认不禁用 false
-        'refresh_disable' => false,
-        // 令牌签发者
-        'iss' => 'webman.tinywan.cn',
-        // 时钟偏差冗余时间，单位秒。建议这个余地应该不大于几分钟。
-        'leeway' => 60,
-        // 单设备登录
-        'is_single_device' => false,
-        // 缓存令牌时间，单位：秒。默认 7 天
-        'cache_token_ttl' => 604800,
-        // 缓存令牌前缀
-        'cache_token_pre' => 'JWT:TOKEN:',
-        // 用户信息模型
-        'user_model' => function($uid){
-            return [];
-        },
 
-        /**
-         * access令牌私钥
-         */
-        'access_private_key' => <<<EOD
+    // 算法类型 HS256、HS384、HS512、RS256、RS384、RS512、ES256、ES384、Ed25519
+    'algorithms' => 'HS256',
+
+    // access令牌秘钥
+    'access_secret_key' => '2022d3d3LmJq',
+
+    // access令牌过期时间，单位：秒。默认 2 小时
+    'access_exp' => 7200,
+
+    // refresh令牌秘钥
+    'refresh_secret_key' => '2022KTxigxc9o50c',
+
+    // refresh令牌过期时间，单位：秒。默认 7 天
+    'refresh_exp' => 604800,
+
+    // refresh 令牌是否禁用，默认不禁用 false
+    'refresh_disable' => false,
+
+    // 令牌签发者
+    'iss' => 'think.tinywan.cn',
+
+    // 时钟偏差冗余时间，单位秒。建议这个余地应该不大于几分钟。
+    'leeway' => 60,
+
+    // 单设备登录
+    'is_single_device' => false,
+
+    // 缓存令牌时间，单位：秒。默认 7 天
+    'cache_token_ttl' => 604800,
+
+    // 缓存令牌前缀
+    'cache_token_pre' => 'JWT:TOKEN:',
+
+    // 用户信息模型
+    'user_model' => function($uid){
+        return [];
+    },
+
+    /**
+     * access令牌私钥
+     */
+    'access_private_key' => <<<EOD
 -----BEGIN RSA PRIVATE KEY-----
 ...
 -----END RSA PRIVATE KEY-----
 EOD,
 
-        /**
-         * access令牌公钥
-         */
-        'access_public_key' => <<<EOD
+    /**
+     * access令牌公钥
+     */
+    'access_public_key' => <<<EOD
 -----BEGIN PUBLIC KEY-----
 ...
 -----END PUBLIC KEY-----
 EOD,
 
-        /**
-         * refresh令牌私钥
-         */
-        'refresh_private_key' => <<<EOD
+    /**
+     * refresh令牌私钥
+     */
+    'refresh_private_key' => <<<EOD
 -----BEGIN RSA PRIVATE KEY-----
 ...
 -----END RSA PRIVATE KEY-----
 EOD,
 
-        /**
-         * refresh令牌公钥
-         */
-        'refresh_public_key' => <<<EOD
+    /**
+     * refresh令牌公钥
+     */
+    'refresh_public_key' => <<<EOD
 -----BEGIN PUBLIC KEY-----
 ...
 -----END PUBLIC KEY-----
-EOD,
-    ],
+EOD
 ];
